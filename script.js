4f3a84a38ee8ab98abeaadeff28c16f7667c07fd
Isabella Rodriguez <157334109+Isa3ellaR@users.noreply.github.com>
2024-02-12 09:58:48 +0000

    updated drawing

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ function setup(){
 }
 
 function draw(){
-  background(200);
+  background(255, 160, 255);
   // add your drawing code here ...
- fill(255, 200, 50)
-  ellipse(150, 50, 50)
+  //Ears
+  stroke(255, 160, 255);
+  arc(225, 120, 100, 100, HALF_PI, TWO_PI);
+  arc(375, 120, 100, 100, PI, HALF_PI);
+  stroke(252, 137, 185);
+  fill(252, 137, 185);
+  ellipse(225, 120, 75);
+  ellipse(375, 120, 75);
+  //Face
+ fill(222, 44, 147);
+  ellipse(300, 200, 175);
+  //eyes
+  stroke(0);
+  ellipse(250, 190, 50);
+  ellipse(350, 190, 50);
+
+  
+ 
+
+
 }
